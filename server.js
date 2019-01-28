f69a85edb743a2273223982aa42b72e0cc6f1ecc
omkar13 <omkarvdamle13@gmail.com>
2019-01-28 09:19:10 -0500

    Basic routes setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const users = require('./routes/api/users'); 
+const profile = require('./routes/api/profile'); 
+const posts = require('./routes/api/posts'); 
+
 const app = express();
 
 //DB configuration
@@ -12,8 +16,14 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
 const port = process.env.port || 5000;
 
 app.get('/', (req,res) => res.send('Hello!!'));
 
 app.listen(port, () => console.log(`server listening on port ${port}`));
+
